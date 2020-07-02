0daa9edb969da35ba8fdf6b3bae239d64afe536a
Dobrica Pavlinusic <dpavlin@rot13.org>
2020-07-02 10:35:43 +0200

    added --all to refresh configurations

--- export-backup.sh
+++ export-backup.sh
@@ -6,10 +6,19 @@ switch=$1
 test -z "$switch" && echo "Usage: $0 switch-name" && exit 1
 
 cd $path
+
+# with --all use existing backups to update configuration
+if [ "$switch" = "--all" ] ; then
+	ls backup/ | sed 's/\.rsc$//' | xargs -i sh -xe $0 {}
+	exit 0
+fi
+
+
+
 ssh -i $path/ssh/mikrotik admin@$switch 'export file="backup"'
 scp -q -i $path/ssh/mikrotik admin@$switch:backup.rsc $path/backup/$switch.rsc
 
 cd $path/backup
-git ls-files --error-unmatch $switch.rsc || git add $switch.rsc && git commit -m "$switch initial config" && exit 0
+git ls-files --error-unmatch $switch.rsc >/dev/null || ( git add $switch.rsc && git commit -m "$switch initial config" && exit 0 )
 
 git diff --stat $switch.rsc | grep '1 file changed, 1 insertion(+), 1 deletion(-)' > /dev/null || git commit -m "$switch `date +%Y-%m-%d`" $switch.rsc
