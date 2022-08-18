dffaab6284e78f015d529d6ec2dd59f794fb9b7f
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-08-18 09:29:30 +0200

    include tilera vlans

--- vlans.pl
+++ vlans.pl
@@ -4,10 +4,11 @@ use strict;
 
 chdir '/home/dpavlin/mikrotik-switch';
 
-foreach my $sw ( glob 'backup/*' ) {
+foreach my $sw ( glob('backup/*.rsc'), glob('../tilera/backup/backup.rsc') ) {
 	open(my $fh, '<', $sw);
-	$sw =~ s{\w+/}{};
+	$sw =~ s{^.*backup/}{};
 	$sw =~ s{\.\w+}{};
+	$sw = 'sw-core' if $sw eq 'backup'; # rewrite tilera name
 	while(<$fh>) {
 		chomp;
 		if ( m/vlan-ids?=(\d+)/ ) {
@@ -19,9 +20,17 @@ foreach my $sw ( glob 'backup/*' ) {
 				$ports = $1;
 			} elsif ( m/ports=(\S+)/ ) {
 				$ports = $1;
+			} elsif (  m/name=(\S+)/ ) {
+				$ports = $1;
 			} else {
 				warn "IGNORED: $_\n";
 			}
+
+			# can be "quoted" and have spaces
+			#if ( m/interface=(\S+)/ ) {
+			#	$ports = "i:$1 $ports";
+			#}
+
 			if ( $ports =~ m/,/ ) {
 				foreach my $port ( split(/,/, $ports) ) {
 					print "$sw $port $vlan\n";
